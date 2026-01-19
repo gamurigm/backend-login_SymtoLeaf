690054f85ecbc08a95264e35a19e143a3a1ed793
Gabriel Murillo Medina <gamurigm@gmail.com>
2026-01-19 15:46:57 -0500

    fix: import ConfigService in AuthModule

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from '../entities/user.entity';
 import { AuthService } from '../services/auth.service';
 import { AuthController } from '../controllers/auth.controller';
